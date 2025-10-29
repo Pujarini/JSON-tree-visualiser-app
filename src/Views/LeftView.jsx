38ab81bd47845a44dd70f5eec95f4c9c0158ef43
Pujarini Jena <pujarinijena@Pujarini-MacBook-Pro.local>
2025-10-29 23:54:26 +0530

    on generate click props

--- src/Views/LeftView.jsx
+++ src/Views/LeftView.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import JSONInputBox from "../components/JSONInputBox";
 import GenerateJSONBtn from "../components/GenerateJSONBtn";
 
-export default function LeftView({ input, onInputChange, isValidJSON, onValidJSONChange }) {
+export default function LeftView({ input, onInputChange, isValidJSON, onValidJSONChange, onGenerate }) {
 
     return (
         <div className="flex flex-col">
             <JSONInputBox value={input} onChange={onInputChange} isValidJSON={isValidJSON} onValidJSONChange={onValidJSONChange} />
-            <GenerateJSONBtn disabledBtn={isValidJSON} />
+            <GenerateJSONBtn disabledBtn={isValidJSON} onClick={onGenerate} />
         </div>
     )
 }
