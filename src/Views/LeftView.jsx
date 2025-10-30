2febf765a8fef805bbd977591e9c611aa672cdc6
Pujarini Jena <pujarinijena@Pujarini-MacBook-Pro.local>
2025-10-30 18:43:48 +0530

    fixed styles fot leftview

--- src/Views/LeftView.jsx
+++ src/Views/LeftView.jsx
@@ -5,7 +5,7 @@ import GenerateJSONBtn from "../components/GenerateJSONBtn";
 export default function LeftView({ input, onInputChange, isValidJSON, onValidJSONChange, onGenerate }) {
 
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-2">
             <JSONInputBox value={input} onChange={onInputChange} isValidJSON={isValidJSON} onValidJSONChange={onValidJSONChange} />
             <GenerateJSONBtn disabledBtn={isValidJSON} onClick={onGenerate} />
         </div>
