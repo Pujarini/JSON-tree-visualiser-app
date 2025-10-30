96ccfc177f0f390e39d25fe2f6c0786d6f58e792
Pujarini Jena <pujarinijena@Pujarini-MacBook-Pro.local>
2025-10-30 18:42:18 +0530

    fixed logic for logic viewer

--- src/components/JSONViewer.jsx
+++ src/components/JSONViewer.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import TreeCanvas from "./Tree-Generator/TreeCanvas";
 import { ReactFlowProvider } from "@xyflow/react"
+import Loader from "./Loader";
 
 
-export default function JSONViewer({ tree, focusNodeId }) {
+export default function JSONViewer({ tree, focusNodeId, loading }) {
+
     return (
-        <div className="mt-6">
-            {tree ? (
-                <ReactFlowProvider>
+        <div className="w-full">
+            {loading ?
+                <div className=" flex min-h-[430px] items-center justify-center rounded-2xl border border-slate-200 bg-white p-4 shadow-inner"><Loader />
+                </div> : tree ? <ReactFlowProvider>
                     <TreeCanvas tree={tree} focusNodeId={focusNodeId} />
-                </ReactFlowProvider>
-            ) : (
-                <div className="mt-6 flex min-h-[430px] items-center justify-center rounded-2xl border border-slate-200 bg-white p-4 shadow-inner">
+                </ReactFlowProvider> : <div className=" flex min-h-[430px] items-center justify-center rounded-2xl border border-slate-200 bg-white p-4 shadow-inner">
                     <div className="text-center text-slate-400">
                         <div className="text-sm">Tree will render here</div>
                     </div>
-                </div>
-            )}
+
+                </div>}
         </div>
     )
 }
